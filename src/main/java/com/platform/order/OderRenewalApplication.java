9b15cdd9c3ef259852a54f5e0fd2fac4cf8a9453
킹받드라슈 <soccer_by@naver.com>
2022-10-24 20:57:30 +0900

    initalize

---
+++ src/main/java/com/platform/order/OderRenewalApplication.java
@@ -0,0 +1,13 @@
+package com.platform.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OderRenewalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OderRenewalApplication.class, args);
+	}
+
+}
