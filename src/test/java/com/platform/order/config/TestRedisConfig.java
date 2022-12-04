6d7e7f478fac4f27c49fa5a35c529848b380da50
킹받드라슈 <soccer_by@naver.com>
2022-12-04 10:58:36 +0900

    fix: embeded redis add

--- src/test/java/com/platform/order/config/TestRedisConfig.java
+++ src/test/java/com/platform/order/config/TestRedisConfig.java
@@ -1,26 +1,44 @@
-package com.platform.order.common.config;
+package com.platform.order.config;
+
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import com.platform.order.common.config.RedisConfig;
 import com.platform.order.common.config.property.RedisProperty;
 
-@Profile({"local"})
+import redis.embedded.RedisServer;
+
 @Configuration
 @EnableRedisRepositories
 @EnableConfigurationProperties(RedisProperty.class)
-public class RedisConfig {
-	private final RedisProperty redisProperty;
+public class TestRedisConfig {
+	private RedisServer redisServer;
+	private RedisProperty redisProperty;
 
-	public RedisConfig(RedisProperty redisProperty) {
+	public TestRedisConfig(RedisProperty redisProperty) {
 		this.redisProperty = redisProperty;
+		redisServer = new RedisServer(redisProperty.port());
+	}
+
+	@PostConstruct
+	public void startRedis() throws IOException {
+		redisServer.start();
+	}
+
+	@PreDestroy
+	public void stopRedis() {
+		redisServer.stop();
 	}
 
 	@Bean
