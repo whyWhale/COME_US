eb37543b158ccf843208d87ed12a004a04a851f0
킹받드라슈 <soccer_by@naver.com>
2023-03-07 22:11:44 +0900

    test: aws 에뮬레이터 테스트 환경 구성

---
+++ src/test/java/com/platform/order/config/LocalStackS3Config.java
@@ -0,0 +1,41 @@
+package com.platform.order.config;
+
+import static org.testcontainers.containers.localstack.LocalStackContainer.Service.S3;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+/**
+ * @Document: aws s3 에뮬레이터
+ */
+@TestConfiguration
+public class LocalStackS3Config {
+	private static final DockerImageName LOCAL_STACK_IMAGE = DockerImageName.parse("localstack/localstack");
+
+	@Value("${cloud.aws.s3.bucket}")
+	private String bucket;
+
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	public LocalStackContainer localStackContainer() {
+		DockerImageName parse = DockerImageName.parse("localstack/localstack");
+		return new LocalStackContainer(LOCAL_STACK_IMAGE)
+			.withServices(LocalStackContainer.Service.S3);
+	}
+
+	@Bean
+	public AmazonS3 amazonS3(LocalStackContainer localStackContainer) {
+		AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard()
+			.withEndpointConfiguration(localStackContainer.getEndpointConfiguration(S3))
+			.withCredentials(localStackContainer.getDefaultCredentialsProvider())
+			.build();
+
+		amazonS3.createBucket(bucket);
+		return amazonS3;
+	}
+}
--- src/test/java/com/platform/order/testenv/IntegrationTest.java
+++ src/test/java/com/platform/order/testenv/IntegrationTest.java
@@ -1,12 +1,16 @@
 package com.platform.order.testenv;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.utility.DockerImageName;
 
+import com.platform.order.config.LocalStackS3Config;
+
 @ActiveProfiles("application-container")
+@Import({LocalStackS3Config.class})
 @SpringBootTest
 public class IntegrationTest {
 	@Container
