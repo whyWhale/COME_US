e22a2d395488d4e5688206bd85c9b94aa36b615d
킹받드라슈 <soccer_by@naver.com>
2023-03-10 02:34:24 +0900

    refactor: validation document 추가

--- src/main/java/com/platform/order/common/validation/KeywordSearchValidator.java
+++ src/main/java/com/platform/order/common/validation/KeywordSearchValidator.java
@@ -3,6 +3,9 @@ package com.platform.order.common.validation;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+/**
+ * keyword 검색을 할때 첫문자 와일드카드를 방지하기 하기 위한 validation
+ */
 public class KeywordSearchValidator implements ConstraintValidator<KeywordSearch, String> {
 
 	@Override
--- src/main/java/com/platform/order/common/validation/MultipartValidator.java
+++ src/main/java/com/platform/order/common/validation/MultipartValidator.java
@@ -5,6 +5,9 @@ import javax.validation.ConstraintValidatorContext;
 
 import org.springframework.web.multipart.MultipartFile;
 
+/**
+ * multipart type 인자의 파일이름 및 파일 사이즈 0일 때를 예방하기 위한 validation
+ */
 public class MultipartValidator implements ConstraintValidator<Multipart, MultipartFile> {
 	@Override
 	public boolean isValid(MultipartFile value, ConstraintValidatorContext context) {
