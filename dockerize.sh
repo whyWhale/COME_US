36e9917983a5621f4b2e003e21993dd3715ebac0
킹받드라슈 <soccer_by@naver.com>
2023-03-24 15:02:37 +0900

    setting: docker 오케스트레이션 nginx, redis(cluster), mysql(master-slave), nginx, application server(2)

--- dockerize.sh
+++ dockerize.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 ./gradlew clean :bootJar
 
-docker build --force-rm -t ${IMG_NAME:-oder-renewal_order-renewal-application} .
+docker build --force-rm -t ${APPLICATION_IMAGE} .
--- run.sh
+++ run.sh
@@ -1,3 +1,27 @@
 #!/usr/bin/env sh
 
+docker network rm come-us
+echo "network create : come-us"
+docker network create come-us
+
+echo "volume remove not dangling! clean .ㅛ."
+# shellcheck disable=SC2046
+docker volume rm $(docker volume ls  -q --filter dangling=true)
+
+echo "mysql redo log delete :)"
+rm -r ./infra/mysql-replication/master/data
+
+echo "docker already duplicate container  remove 'ㅅ' "
+docker rm -f nginx mysql-master mysql-slave-1 mysql-phpmyadmin redis redis-commander redis-node1 redis-node2 redis-node3 redis-cluster-entry localstack come-us-application1 come-us-application2
+
+chmod +x dockerize.sh
+
+echo "application image build"
+export APPLICATION_IMAGE=come-us-application;
+
+./dockerize.sh
+
+echo "docker up ready :)"
 docker-compose up -d
+
+echo "executing application and infra '-' "
