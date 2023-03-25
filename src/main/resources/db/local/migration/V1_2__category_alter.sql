9edddc890dc1cce10fd056ab90b8a28ba58de734
킹받드라슈 <soccer_by@naver.com>
2023-03-26 00:53:20 +0900

    refactor: flyway 환경별 seed, migrate 분리

---
+++ src/main/resources/db/local/migration/V1_2__category_alter.sql
@@ -0,0 +1,11 @@
+alter table category
+    add parent_id bigint null;
+
+alter table category
+    modify parent_id bigint null after id;
+
+alter table category
+    modify code varchar(255) not null;
+
+create unique index category_code_uindex
+    on category (code);
