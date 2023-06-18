58e559181f2a48fdc9aea0a58a55c77cab276e2f
킹받드라슈 <soccer_by@naver.com>
2023-06-18 21:22:13 +0900

    refactor: 스케줄 스레드풀 async 추가

--- src/main/java/com/platform/order/common/config/ScheduleConfig.java
+++ src/main/java/com/platform/order/common/config/ScheduleConfig.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
@@ -15,14 +19,27 @@ import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 @Profile({"local", "prod"})
 @EnableSchedulerLock(defaultLockAtMostFor = "PT20S", defaultLockAtLeastFor = "PT20S")
 @Configuration
+@EnableAsync
 @EnableScheduling
-public class ScheduleConfig {
+public class ScheduleConfig implements SchedulingConfigurer {
 	@Bean
 	public LockProvider lockProvider(
 		@Qualifier("masterDataSource")
 		DataSource dataSource
 	) {
 		return new JdbcTemplateLockProvider(dataSource);
+	}
+
+	@Override
+	public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+		ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+
+		threadPoolTaskScheduler.setPoolSize(5);
+		threadPoolTaskScheduler.setThreadGroupName("scheduler thread pool");
+		threadPoolTaskScheduler.setThreadNamePrefix("scheduler-thread-");
+		threadPoolTaskScheduler.initialize();
+
+		taskRegistrar.setTaskScheduler(threadPoolTaskScheduler);
 	}
 
 }
